811b90dec3f5ff8b9ef45daf805d5a2117647786
pudelkoM <pudelkoM@users.noreply.github.com>
2025-06-22 13:41:39 +0200

    Use clang compiler and set CFLAGS

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 
 set -exo pipefail
 
-env CGO_ENABLED="1" CC=cc CXX=c++ GOOS=darwin GOARCH=arm64 go build -o ./bin/$(basename $PWD)_darwin_arm64 ./main.go
-
+env CGO_ENABLED="1" CGO_CFLAGS="-O3 -g -march=native" CGO_CXXFLAGS="-O3 -g -march=native" CC=clang CXX=clang++ GOOS=darwin GOARCH=arm64 go build -o ./bin/$(basename $PWD)_darwin_arm64 ./main.go
